355779287533e91c0fe60f67e9dcf1278d4c4042
Ron McLaren <ronlaren@aol.com>
2024-03-08 18:20:31 -0500

    added tests

---
+++ test/testinput/bufrtest_python_test.py
@@ -0,0 +1,227 @@
+# (C) Copyright 2023 NOAA/NWS/NCEP/EMC
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+import pyioda
+from pyioda.ioda.Engines import bufr
+import numpy as np
+
+# bufr = pyioda.ioda.Engines.bufr
+
+def test_basic_query():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t00z.1bhrs4.tm00.bufr_d'
+
+    # Make the QuerySet for all the data we want
+    q = bufr.QuerySet()
+    q.add('year', '*/YEAR')
+    q.add('month', '*/MNTH')
+    q.add('day', '*/DAYS')
+    q.add('hour', '*/HOUR')
+    q.add('minute', '*/MINU')
+    q.add('second', '*/SECO')
+    q.add('latitude', '*/CLON')
+    q.add('longitude', '*/CLAT')
+    q.add('radiance', '*/BRIT{1}/TMBR')
+    q.add('radiance_all', '*/BRIT/TMBR')
+
+    # Open the BUFR file and execute the QuerySet
+    with bufr.File(DATA_PATH) as f:
+        r = f.execute(q)
+
+    import inspect
+
+    print(pyioda.__file__)
+
+    # Use the ResultSet returned to get numpy arrays of the data
+    print(type(r.get('latitude')))
+    lat = r.get('latitude')
+    rad = r.get('radiance')
+    rad_all = r.get('radiance_all')
+
+    # Validate the values that were returned
+    assert np.allclose(lat[0:3], np.array([166.7977, 166.4078, 165.992]))
+    assert np.allclose(rad[0:3], np.array([198.69, 254.06, 233.85]))
+    assert len(rad_all.shape) == 2
+
+    datetimes = r.get_datetime('year', 'month', 'day', 'hour', 'minute', 'second')
+    assert datetimes[5] == np.datetime64('2020-10-26T21:00:01')
+    assert datetimes.fill_value == np.datetime64('1970-01-01T00:00:00')
+
+
+def test_string_field():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t12z.adpupa.tm00.bufr_d'
+
+    # Make the QuerySet for all the data we want
+    q = bufr.QuerySet()
+    q.add('borg', '*/BID/BORG')
+
+    # Open the BUFR file and execute the QuerySet
+    with bufr.File(DATA_PATH) as f:
+        r = f.execute(q)
+
+    # Use the ResultSet returned to get numpy arrays of the data
+    borg = r.get('borg')
+
+    # Validate the values that were returned
+    assert (np.all(borg[0][0:3] == ['KWBC', 'KWBC', 'KAWN']))
+
+
+def test_long_str_field():
+    DATA_PATH ='Data/testinput_tier_1/gdas.t06z.snocvr.tm00.bufr_d'
+
+    # Make the QuerySet for all the data we want
+    q = bufr.QuerySet()
+    q.add('lid', '*/WGOSLID')
+
+    # Open the BUFR file and execute the QuerySet
+    with bufr.File(DATA_PATH) as f:
+        r = f.execute(q)
+
+    # Use the ResultSet returned to get numpy arrays of the data
+    lid = r.get('lid')
+
+    # Validate the values that were returned
+    assert (lid[0] == '570282')
+    assert (lid[6] == '613180')
+    assert (np.all(lid[0:7].mask == [False, True, True, True, True, True, False]))
+
+def test_type_override():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t00z.1bhrs4.tm00.bufr_d'
+
+    # Make the QuerySet for all the data we want
+    q = bufr.QuerySet()
+    q.add('day', '*/DAYS')
+    q.add('longitude', '*/CLAT')
+
+    # Open the BUFR file and execute the QuerySet
+    with bufr.File(DATA_PATH) as f:
+        r = f.execute(q)
+
+    day = r.get('day')
+    day_float = r.get('day', type='float')
+
+    assert day.dtype == 'int32'
+    assert day_float.dtype == 'float32'
+
+    lat = r.get('longitude')
+    lat_int = r.get('longitude', type='int')
+
+    assert lat.dtype == 'float32'
+    assert lat_int.dtype == 'int32'
+    assert lat_int.fill_value == 2147483647  # the max int32 value
+
+def test_invalid_query():
+    q = bufr.QuerySet()
+
+    try:
+        q.add('latitude', '!!! */CLON')
+    except Exception as e:
+        return
+
+    assert False, "Didn't throw exception for invalid query."
+
+
+def test_highlevel_replace():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t00z.1bhrs4.tm00.bufr_d'
+    YAML_PATH = 'testinput/iodatest_bufr_hrs_basic_mapping.yaml'
+
+    container = bufr.Parser(DATA_PATH, YAML_PATH).parse()
+
+    data = container.get('variables/brightnessTemp')
+    container.replace('variables/brightnessTemp', data * 0.1)
+
+    obsgroup = next(iter(bufr.IodaEncoder(YAML_PATH).encode(container).values()))
+
+    obs_temp = obsgroup.vars.open("ObsValue/brightnessTemperature").readNPArray.float()
+
+    assert obs_temp.shape == data.shape
+    assert np.allclose(obs_temp, data * 0.1)
+
+def test_highlevel_add():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t00z.1bhrs4.tm00.bufr_d'
+    YAML_PATH = 'testinput/iodatest_bufr_hrs_basic_mapping.yaml'
+
+    container = bufr.Parser(DATA_PATH, YAML_PATH).parse()
+
+    data = container.get('variables/brightnessTemp')
+    paths = container.getPaths('variables/brightnessTemp')
+    container.add('variables/brightnessTemp_new', data, paths)
+
+    iodaDescription = bufr.IodaDescription(YAML_PATH)
+    iodaDescription.add_variable(name='ObsValue/new_brightnessTemperature',
+                                 source='variables/brightnessTemp_new',
+                                 units='K',
+                                 longName='New Brightness Temperature')
+
+    obsgroup = next(iter(bufr.IodaEncoder(iodaDescription).encode(container).values()))
+
+    obs_orig = obsgroup.vars.open("ObsValue/brightnessTemperature").readNPArray.float()
+    obs_temp = obsgroup.vars.open("ObsValue/new_brightnessTemperature").readNPArray.float()
+
+    assert np.allclose(obs_temp, obs_orig)
+    assert obs_temp.shape == data.shape
+
+def test_highlevel_w_category():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t12z.1bamua.tm00.bufr_d'
+    YAML_PATH = 'testinput/iodatest_bufr_amua_ta_mapping.yaml'
+
+    container = bufr.Parser(DATA_PATH, YAML_PATH).parse()
+
+    categories = container.allSubCategories()
+    for category in categories:  # [metop-a]
+        data = container.get('variables/antennaTemperature', category)
+        paths = container.getPaths('variables/antennaTemperature', category)
+        container.add('variables/antennaTemperature1', data, paths, category)
+
+    iodaDescription = bufr.IodaDescription(YAML_PATH)
+    iodaDescription.add_variable(name='ObsValue/brightnessTemperature_new',
+                                 source='variables/antennaTemperature1',
+                                 units='K')
+
+    # get the obsgroup for the first category
+    obs_group = next(iter(bufr.IodaEncoder(iodaDescription).encode(container).values()))
+
+    assert np.allclose(obs_group.vars.open('ObsValue/brightnessTemperature_new').readVector.float(),
+                       obs_group.vars.open('ObsValue/brightnessTemperature').readVector.float())
+
+def test_highlevel_cache():
+    DATA_PATH = 'Data/testinput_tier_1/gdas.t12z.1bamua.tm00.bufr_d'
+    YAML_PATH = 'testinput/iodatest_bufr_amua_ta_mapping.yaml'
+
+    if not bufr.DataCache.has(DATA_PATH, YAML_PATH):
+        dat = bufr.Parser(DATA_PATH, YAML_PATH).parse()
+        bufr.DataCache.add(DATA_PATH, YAML_PATH, dat.allSubCategories(), dat)
+    else:
+        assert False, "Data Cache incorrect."
+
+    if not bufr.DataCache.has(DATA_PATH, YAML_PATH):
+        assert False, "Data Cache does not contain entry."
+
+    container = bufr.DataCache.get(DATA_PATH, YAML_PATH)
+
+    categories = container.allSubCategories()
+    for category in categories:
+        cache_dat = bufr.DataCache.get(DATA_PATH, YAML_PATH)
+        assert np.allclose(dat.get('variables/antennaTemperature', category),
+                           cache_dat.get('variables/antennaTemperature', category))
+
+        bufr.DataCache.mark_finished(DATA_PATH, YAML_PATH, category)
+
+    if bufr.DataCache.has(DATA_PATH, YAML_PATH):
+        assert False, "Data Cache still contains entry."
+
+
+if __name__ == '__main__':
+    # Low level interface tests
+    test_basic_query()
+    test_string_field()
+    test_long_str_field()
+    test_type_override()
+    test_invalid_query()
+
+    # High level interface tests
+    test_highlevel_replace()
+    test_highlevel_add()
+    test_highlevel_w_category()
+    test_highlevel_cache()
---
+++ tools/bufr2ioda/bufr2ioda.cpp
@@ -0,0 +1,164 @@
+/*
+ * (C) Copyright 2020 NOAA/NWS/NCEP/EMC
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include <string>
+#include <iostream>
+#include <ostream>
+
+#include "eckit/config/YAMLConfiguration.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/filesystem/PathName.h"
+
+#include "bufr/BufrParser.h"
+#include "bufr/IodaDescription.h"
+#include "bufr/IodaEncoder.h"
+
+#include "ioda/Group.h"
+#include "ioda/Engines/EngineUtils.h"
+#include "ioda/ObsGroup.h"
+#include "ioda/Layout.h"
+#include "ioda/Copying.h"
+#include "ioda/Misc/DimensionScales.h"
+
+namespace bufr
+{
+//    typedef ObjectFactory<Parser, const eckit::LocalConfiguration&> ParseFactory;
+
+    void parse(const std::string& obsFile,
+               const std::string& mappingFile,
+               const std::string& outputFile,
+               const std::string& tablePath = "",
+               std::size_t numMsgs = 0)
+    {
+        std::unique_ptr<eckit::YAMLConfiguration>
+            yaml(new eckit::YAMLConfiguration(eckit::PathName(mappingFile)));
+
+        if (yaml->has("ioda"))
+        {
+            auto data = BufrParser(obsFile,
+                                   yaml->getSubConfiguration("bufr"), tablePath).parse(numMsgs);
+
+            auto dataMap = IodaEncoder(yaml->getSubConfiguration("ioda")).encode(data);
+
+            ioda::Engines::BackendCreationParameters backendParams;
+            backendParams.fileName = outputFile;
+            backendParams.openMode = ioda::Engines::BackendOpenModes::Read_Write;
+            backendParams.createMode = ioda::Engines::BackendCreateModes::Truncate_If_Exists;
+            backendParams.action = ioda::Engines::BackendFileActions::Create;
+            backendParams.flush = true;
+
+            auto rootGroup = ioda::Engines::constructBackend(ioda::Engines::BackendNames::Hdf5File,
+                                                         backendParams);
+
+            const auto& obsGroup = dataMap.begin()->second;
+            ioda::copyGroup(obsGroup, rootGroup);
+        }
+        else
+        {
+            eckit::BadParameter("No section named \"observations\"");
+        }
+    }
+}  // namespace bufr
+
+
+static void showHelp()
+{
+    std::cerr << "Usage: bufr2ioda.x [-n NUM_MESSAGES] YAML_PATH"
+              << "Options:\n"
+              << "  -h,  Show this help message\n"
+              << "  -n NUM_MESSAGES,  Number of BUFR messages to parse."
+              << std::endl;
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 4)
+    {
+        showHelp();
+        return 0;
+    }
+
+    std::string obsFile;
+    std::string mappingFile;
+    std::string outputFile;
+    std::string tablePath = "";
+    std::size_t numMsgs = 0;
+
+    enum class ReqArgType
+    {
+        ObsFile = 0,
+        MappingFile = 1,
+        OutputFile = 2
+    };
+
+    auto reqArgIdx = ReqArgType::ObsFile;
+    std::size_t argIdx = 1;
+    while (argIdx < static_cast<std::size_t> (argc))
+    {
+        if (strcmp(argv[argIdx], "-n") == 0)
+        {
+            if (static_cast<std::size_t> (argc) > argIdx + 1)
+            {
+                numMsgs = atoi(argv[argIdx + 1]);
+            }
+            else
+            {
+                showHelp();
+                return 0;
+            }
+
+            argIdx += 2;
+        }
+        else if (strcmp(argv[argIdx], "-h") == 0)
+        {
+            showHelp();
+            return 0;
+        }
+        else if (strcmp(argv[argIdx], "-t") == 0)
+        {
+            if (static_cast<std::size_t> (argc) > argIdx + 1)
+            {
+                tablePath = std::string(argv[argIdx + 1]);
+            }
+            else
+            {
+                showHelp();
+                return 0;
+            }
+
+            argIdx += 2;
+        }
+        else
+        {
+            switch (reqArgIdx)
+            {
+                case ReqArgType::ObsFile:
+                    obsFile = std::string(argv[argIdx]);
+                    break;
+                case ReqArgType::MappingFile:
+                    mappingFile = std::string(argv[argIdx]);
+                    break;
+                case ReqArgType::OutputFile:
+                    outputFile = std::string(argv[argIdx]);
+                    break;
+                default:
+                    break;
+            }
+
+            argIdx++;
+            reqArgIdx = static_cast<ReqArgType> (static_cast<int> (reqArgIdx) + 1);
+        }
+    }
+
+    std::cout << "outputFile: " << outputFile << std::endl;
+
+    bufr::parse(obsFile, mappingFile, outputFile, tablePath, numMsgs);
+
+    return 0;
+}
+
