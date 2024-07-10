b9c69946c5c4833c362da056a3653609e1fd36e8
Ron McLaren <ronlaren@aol.com>
2024-07-10 11:53:36 -0400

    tweak, checnaged time elapsed msg

--- tools/bufr2netcdf/bufr2netcdf.cpp
+++ tools/bufr2netcdf/bufr2netcdf.cpp
@@ -97,7 +97,7 @@ namespace mpi {
         eckit::BadParameter("No section named \"encoder\"");
     }
 
-    logElapsedTime("Finished", startTime);
+    logElapsedTime("Total Time", startTime);
   }
 
   void parse(const eckit::mpi::Comm& comm,
@@ -150,7 +150,7 @@ namespace mpi {
     comm.barrier();
     if (comm.rank() == 0)
     {
-      logElapsedTime("Finished", startTime);
+      logElapsedTime("Total Time", startTime);
     }
   }
 }  // namespace bufr
