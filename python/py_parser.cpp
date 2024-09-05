750622cfb580515371630a72899892c7df3f9d4b
Ron McLaren <ronlaren@aol.com>
2024-09-05 14:30:10 -0400

    Added RunParameters to python interface

--- python/py_parser.cpp
+++ python/py_parser.cpp
@@ -20,27 +20,35 @@
 namespace py = pybind11;
 
 using bufr::BufrParser;
+using bufr::RunParameters;
 
 void setupParser(py::module& m)
 {
   m.doc() = "Provides the ability to process data from BUFR files.";
+
+  py::class_<RunParameters>(m, "RunParameters")
+    .def(py::init<>())
+    .def_readwrite("offset", &RunParameters::offset)
+    .def_readwrite("num_messages", &RunParameters::numMessages)
+    .def_readwrite("start_time", &RunParameters::startTime)
+    .def_readwrite("stop_time", &RunParameters::stopTime);
 
   py::class_<BufrParser>(m, "Parser")
     .def(py::init<const std::string&, const std::string&, const std::string&>(),
          py::arg("obsfile"),
          py::arg("mapping_path"),
          py::arg("table_path") = "")
-    .def("parse", [](BufrParser& self, const bufr::RunParameters& params)
+    .def("parse", [](BufrParser& self, const RunParameters& params)
          {
            return self.parse(params);
          },
-         py::arg("params") = bufr::RunParameters(),
+         py::arg("params") = RunParameters(),
          "Get Parser to parse a config file and get the data container.")
-    .def("parse", [](BufrParser& self, bufr::mpi::Comm& comm, const bufr::RunParameters& params)
+    .def("parse", [](BufrParser& self, bufr::mpi::Comm& comm, const RunParameters& params)
         {
           return self.parse(comm.getComm(), params);
         },
         py::arg("comm"),
-        py::arg("params") = bufr::RunParameters(),
+        py::arg("params") = RunParameters(),
         "Get Parser to parse a config file and get the data container in parallel.");
 }
