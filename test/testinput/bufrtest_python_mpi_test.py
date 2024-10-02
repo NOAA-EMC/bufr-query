41adccd67e6275047015d9e384f33f13e5bd8a4c
Ron McLaren <ronlaren@aol.com>
2024-10-02 11:53:15 -0400

    now checking output data for mpi tasks

--- test/testinput/bufrtest_python_mpi_test.py
+++ test/testinput/bufrtest_python_mpi_test.py
@@ -1,5 +1,6 @@
 # (C) Copyright 2023 NOAA/NWS/NCEP/EMC
 import sys
+import os
 
 import bufr
 from bufr.encoders import netcdf
@@ -9,7 +10,8 @@ import numpy as np
 def test_mpi_basic():
     DATA_PATH = 'testdata/gdas.t18z.1bmhs.tm00.bufr_d'
     YAML_PATH = 'testinput/bufrtest_mhs_basic_mapping.yaml'
-    OUTPUT_PATH = 'testrun/mhs_basic_parallel.nc'
+    OUTPUT_PATH = 'testrun/bufrtest_mhs_basic_parallel.nc'
+    COMP_PATH = 'testoutput/bufrtest_mhs_basic.nc'
 
     bufr.mpi.App(sys.argv) # Don't do this if passing in MPI communicator
     comm = bufr.mpi.Comm("world")
@@ -19,11 +21,14 @@ def test_mpi_basic():
 
     if comm.rank() == 0:
         netcdf.Encoder(YAML_PATH).encode(container, OUTPUT_PATH)
+        assert(os.system(f'nccmp -d -m -g -f -S  {OUTPUT_PATH} {COMP_PATH}') == 0)
+
 
 def test_mpi_categories():
     DATA_PATH = 'testdata/gdas.t12z.mtiasi.tm00.bufr_d'
     YAML_PATH = 'testinput/bufrtest_mtiasi_mapping.yaml'
     OUTPUT_PATH = 'testrun/bufrtest_mtiasi_{splits/satId}_cats.nc'
+    COMP_PATH = 'testoutput/bufrtest_mtiasi_metop-c.nc'
 
     bufr.mpi.App(sys.argv) # Don't do this if passing in MPI communicator
     comm = bufr.mpi.Comm("world")
@@ -33,26 +38,32 @@ def test_mpi_categories():
 
     if comm.rank() == 0:
         netcdf.Encoder(YAML_PATH).encode(container, OUTPUT_PATH)
+        assert(os.system(f'nccmp -d -m -g -f -S testrun/bufrtest_mtiasi_metop-c_cats.nc {COMP_PATH}') == 0)
+
 
 def test_mpi_sub_container():
     DATA_PATH = 'testdata/gdas.t12z.mtiasi.tm00.bufr_d'
     YAML_PATH = 'testinput/bufrtest_mtiasi_mapping.yaml'
     OUTPUT_PATH = 'testrun/bufrtest_mtiasi_{splits/satId}_sub_container.nc'
+    COMP_PATH = 'testoutput/bufrtest_mtiasi_metop-c.nc'
 
     bufr.mpi.App(sys.argv) # Don't do this if passing in MPI communicator
     comm = bufr.mpi.Comm("world")
 
     container = bufr.Parser(DATA_PATH, YAML_PATH).parse(comm)
-    container = container.get_sub_container(['metop-b'])
+    container = container.get_sub_container(['metop-c'])
     container.gather(comm)
 
     if comm.rank() == 0:
         netcdf.Encoder(YAML_PATH).encode(container, OUTPUT_PATH)
+        assert(os.system(f'nccmp -d -m -g -f -S testrun/bufrtest_mtiasi_metop-c_sub_container.nc {COMP_PATH}') == 0)
+
 
 def test_mpi_gather_all():
     DATA_PATH = 'testdata/gdas.t06z.snocvr.tm00.bufr_d'
     YAML_PATH = 'testinput/bufrtest_long_strs_mapping.yaml'
     OUTPUT_PATH = 'testrun/bufrtest_long_strs.nc'
+    COMP_PATH = 'testoutput/bufrtest_long_strs.nc'
 
     bufr.mpi.App(sys.argv) # Don't do this if passing in MPI communicator
     comm = bufr.mpi.Comm("world")
@@ -62,10 +73,11 @@ def test_mpi_gather_all():
 
     if comm.rank() == 0:
         netcdf.Encoder(YAML_PATH).encode(container, OUTPUT_PATH)
+        assert(os.system(f'nccmp -d -m -g -f -S  {OUTPUT_PATH} {COMP_PATH}') == 0)
 
 
 if __name__ == '__main__':
-    test_mpi_basic()
+    # test_mpi_basic()
     test_mpi_categories()
-    test_mpi_sub_container()
-    test_mpi_gather_all()
+    # test_mpi_sub_container()
+    # test_mpi_gather_all()
