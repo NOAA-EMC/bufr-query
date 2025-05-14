c60a91fd356f5b78aa2f462e787bc4bc043f91c1
emilyhcliu <36091766+emilyhcliu@users.noreply.github.com>
2025-05-14 13:36:20 -0400

    Move compute_solar_angles and get_default_nprocs to top levels f obs builder (#74)
    * add map_path and create bufr.transform.wind python package in obs_builder
    
    * Add bufr.transforms.geometry package and add get_default_nprocs to
    bufr.obs_builder package
    
    * change get_default_nprocs to nprocs_per_task
    
    ---------
    
    Co-authored-by: Emily Liu <eliu@hercules-login-4.hpc.msstate.edu>
    Co-authored-by: Emily Liu <eliu@hercules-login-3.hpc.msstate.edu>
    Co-authored-by: Emily Liu <eliu@hercules-login-2.hpc.msstate.edu>

--- python/bufr/obs_builder/__init__.py
+++ python/bufr/obs_builder/__init__.py
@@ -1,5 +1,7 @@
 from .logger import Logger
 
+from .nprocs_per_task import nprocs_per_task
+from .map_path import map_path 
 from .add_main_functions import add_main_functions
 from .obs_builder import ObsBuilder
 from .obs_builder import add_encoder_type
