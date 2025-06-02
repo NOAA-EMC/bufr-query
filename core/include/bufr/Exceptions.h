a901f13e7e35c339e4116ac43c0bf29b49a90e87
rmclaren <ronald.mclaren@noaa.gov>
2025-06-02 17:46:37 -0400

    Move custom exceptions header to public interface (#102)

---
+++ core/include/bufr/Exceptions.h
@@ -0,0 +1,31 @@
+// (C) Copyright 2024 NOAA/NWS/NCEP/EMC
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace bufr {
+
+class Exception : public std::runtime_error {
+ public:
+  explicit Exception(const std::string& what) : std::runtime_error(what) {}
+};
+
+class BadParameter : public Exception {
+ public:
+  explicit BadParameter(const std::string& what) : Exception(what) {}
+};
+
+class BadValue : public Exception {
+ public:
+  explicit BadValue(const std::string& what) : Exception(what) {}
+};
+
+class MissingData : public Exception {
+ public:
+  explicit MissingData(const std::string& what) : Exception(what) {}
+};
+
+}  // namespace bufr
+
