a6542c70f62be7de60e04abd8ddd4123f471c53f
Ron McLaren <ronald.mclaren@noaa.gov>
2026-01-05 16:35:09 +0000

    undid unrelated bugfix

--- core/src/bufr/DataContainer.cpp
+++ core/src/bufr/DataContainer.cpp
@@ -245,12 +245,6 @@ namespace bufr {
 
   void DataContainer::append(const DataContainer& other)
   {
-    // The other DataContainer is empty, nothing to do.
-    if (other.size() == 0)
-    {
-      return;
-    }
-
     bool isEmpty = getFieldNames().empty();
 
     for (const auto &subCat: other.allSubCategories())
